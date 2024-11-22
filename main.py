fa80b3d92409d1e05f5b1fa96b148cbb44c09e78
Aamanus <aamanus@gmail.com>
2024-11-22 15:36:06 +0000

    Functional movment of player

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import pygame
 from constants import *
+from player import Player
+
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-
+    pclock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables,drawables)
+    p1 = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for obj in updatables:
+            obj.update(dt)
         screen.fill((0, 0, 0))
+        for obj in drawables:
+            obj.draw(screen)
         pygame.display.flip()
+        dt = pclock.tick(60)/1000
+
+
 
         
 
